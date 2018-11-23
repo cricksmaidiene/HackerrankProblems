20d47c2b51abcf4f22d92e8d6a4cabd52981a01c
Eshwaran Venkat <32425312+eash-98@users.noreply.github.com>
2018-11-23 16:50:18 +0400

    Create Kangaroo.cpp
    https://www.hackerrank.com/challenges/kangaroo/

---
+++ Kangaroo.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+string kangaroo(int x1, int v1, int x2, int v2) {
+    if((x1<x2 && v1<=v2) || (x2<x1 && v2<=v1))
+       return "NO";
+    else{
+        if((x1-x2)%(v2-v1)==0)
+        return "YES";
+        else
+        return "NO";
+    }    
+}    
+
+int main() {
+    int x1;
+    int v1;
+    int x2;
+    int v2;
+    cin >> x1 >> v1 >> x2 >> v2;
+    string result = kangaroo(x1, v1, x2, v2);
+    cout << result << endl;
+    return 0;
+}
+
